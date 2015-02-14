4645b3bf003a50587ca3195e21fc4a75bde247a9
James Goppert <james.goppert@gmail.com>
2015-02-13 19:31:55 -0500

    Increased process noise.

--- src/modules/local_position_estimator/params.c
+++ src/modules/local_position_estimator/params.c
@@ -5,5 +5,5 @@ PARAM_DEFINE_FLOAT(LPE_SD_FLW_V, 1.0e-2f); // std dev for flow velocity
 PARAM_DEFINE_FLOAT(LPE_SD_FLW_Z, 2.0e-2f); // flow z
 PARAM_DEFINE_FLOAT(LPE_SD_LDR_Z, 5.0e-2f); // lidar z
 PARAM_DEFINE_FLOAT(LPE_SD_ACC_XY, 1.0e-3f); // accel xy
-PARAM_DEFINE_FLOAT(LPE_SD_ACC_Z, 1.0e-2f); // accel z
+PARAM_DEFINE_FLOAT(LPE_SD_ACC_Z, 1.0e-1f); // accel z
 PARAM_DEFINE_FLOAT(LPE_SD_BAR_Z, 5.0f); // baro z
