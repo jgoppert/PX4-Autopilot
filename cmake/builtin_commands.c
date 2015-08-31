e901ab26ca99339d9116c5eaae1f8c9fe681c324
James Goppert <james.goppert@gmail.com>
2015-08-31 08:28:58 -0400

    Work on nuttx build. Hijacked makefile for cmake build.

--- cmake/builtin_commands.c
+++ cmake/builtin_commands.c
@@ -1,6 +1,9 @@
 /* builtin command list - automatically generated, do not edit */
 #include <nuttx/config.h>
 #include <nuttx/binfmt/builtin.h>
+#include <nuttx/config.h>
+uint8_t romfs_img_len = 0;
+uint8_t romfs_img[] = {};
 extern int px4_simple_app_main(int argc, char *argv[]);
 const struct builtin_s g_builtins[] = {
     {"px4_simple_app", SCHED_PRIORITY_DEFAULT, 1024, px4_simple_app_main},
